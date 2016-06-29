1d255253d20b306c7f54d08672f6c504e8c87d73
Aelith Blanchett <tonyteur@yahoo.fr>
2016-06-29 11:38:21 -0400

    updated index.js to fit version 0.3.0

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var FChatLib = require('fchatlib');
-var myFchatBot = new FChatLib(__dirname+'/config/config.js'); //it auto-loads the NSFW.js plugin, and the config has been made private.
-myFchatBot.connect();
+var config = require(__dirname+'/config/config.js');
+var myFchatBot = new FChatLib(); //it auto-loads the NSFW.js plugin, and the config has been made private.
 console.log("Bot successfully started");
