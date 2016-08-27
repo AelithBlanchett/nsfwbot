62b18efd8cf714d80a90d8c25ace5a0ea93eb167
Aelith Blanchett <tonyteur@yahoo.fr>
2016-08-26 20:33:30 -0400

    added a whole lot of things. too many, infact

--- tests/tests.ts
+++ tests/tests.ts
@@ -1,4 +1,43 @@
 import {Fighter} from "../plugins/NSFWClasses/Fighter";
 import {Fight} from "../plugins/NSFWClasses/Fight";
-let myFighter = new Fighter("test", 0, 0 ,0 ,0 ,0 ,0);
+//let myFighter = new Fighter("test", 0, 0 ,0 ,0 ,0);
+
+var Jasmine = require('jasmine');
+var jasmine = new Jasmine();
+
+/// <reference path="../typings/jasmine/jasmine.d.ts">
+
+describe("Fighter testing", () => {
+
+    //it("should say that it doesn't exist", function(done) {
+    //    Fighter.exists("test").then(function(result) {
+    //        done();
+    //    }).catch(function(err) {
+    //        done.fail(err);
+    //    });
+    //});
+    //var fakeFighter = null;
+    //
+    //beforeEach(function() {
+    //    fakeFighter = {
+    //        create: function(){
+    //        }
+    //    };
+    //
+    //    spyOn(fakeFighter, 'create');
+    //});
+    //
+    //it("should fail as no name was passed", function(){
+    //    expect(function(){new Fighter();}).toThrowError();
+    //});
+    //
+    //it("should call create", function(){
+    //    var mySpy = spyOn(Fighter.prototype, 'create').and.callFake(function(){console.log("Create called")});
+    //    var fightur = new Fighter("test45f");
+    //    expect(mySpy).toHaveBeenCalled();
+    //});
+});
+
+jasmine.execute();
+
 //console.log(myFighter.name);
