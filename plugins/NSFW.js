aa9c97031b5a987c3b54ca057764abefa4832be1
AelithBlanchett <tonyteur@yahoo.fr>
2016-10-02 23:06:56 -0400

    Delete NSFW.js

--- plugins/NSFW.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const CommandHandler_1 = require("./NSFWClasses/CommandHandler");
-module.exports = function (parent, channel) {
-    let cmdHandler = new CommandHandler_1.CommandHandler(parent, channel);
-    return cmdHandler;
-};
-//# sourceMappingURL=NSFW.js.map
