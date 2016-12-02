aa5869a6b6061fd479087e712b7502d8bba08d4f
Aelith Blanchett <tonyteur@yahoo.fr>
2016-12-02 16:44:26 -0500

    updated views and models

--- src/SQL/nsfw_fighterview.sql
+++ src/SQL/nsfw_fighterview.sql
@@ -1,8 +1,8 @@
-SELECT
+SELECT fighters.name, fighters.areStatsPrivate, fighters.dexterity,
   (
     SELECT Count(idFight)
     FROM nsfw_activefighters
-    WHERE idFighter = 'Aelith'
+    WHERE idFighter = fighters.name
   )                                        AS fightsCount,
 
   (
@@ -10,13 +10,13 @@ SELECT
     FROM nsfw_activefighters af
       LEFT JOIN nsfw_fights f
         ON af.idFight = f.idFight
-    WHERE f.season = 1
-          AND idFighter = 'Aelith'
+    WHERE f.season = consts.value
+          AND idFighter = fighters.name
   )                                        AS fightsCountCS,
   (
     SELECT Count(idFight)
     FROM nsfw_activefighters
-    WHERE idFighter = 'Aelith' AND fightStatus = 0
+    WHERE idFighter = fighters.name AND fightStatus = 0
   )                                        AS losses,
   (
     SELECT Count(af.idFight)
@@ -24,13 +24,13 @@ SELECT
       LEFT JOIN nsfw_fights f
         ON af.idFight = f.idFight
     WHERE f.season = 1
-          AND idFighter = 'Aelith'
+          AND idFighter = fighters.name
           AND fightStatus = 0
   )                                        AS lossesSeason,
   (
     SELECT Count(idFight)
     FROM nsfw_activefighters
-    WHERE idFighter = 'Aelith' AND fightStatus = 1
+    WHERE idFighter = fighters.name AND fightStatus = 1
   )                                        AS wins,
   (
     SELECT Count(af.idFight)
@@ -38,13 +38,13 @@ SELECT
       LEFT JOIN nsfw_fights f
         ON af.idFight = f.idFight
     WHERE f.season = 1
-          AND idFighter = 'Aelith'
+          AND idFighter = fighters.name
           AND fightStatus = 1
   )                                        AS winsSeason,
   (
     SELECT Count(idFight)
     FROM nsfw_activefighters
-    WHERE idFighter = 'Aelith' AND fightStatus = 2
+    WHERE idFighter = fighters.name AND fightStatus = 2
   )                                        AS currentlyPlaying,
   (
     SELECT Count(af.idFight)
@@ -52,13 +52,13 @@ SELECT
       LEFT JOIN nsfw_fights f
         ON af.idFight = f.idFight
     WHERE f.season = 1
-          AND idFighter = 'Aelith'
+          AND idFighter = fighters.name
           AND fightStatus = 0
   )                                        AS currentlyPlayingSeason,
   (
     SELECT Count(idFight)
     FROM nsfw_activefighters
-    WHERE idFighter = 'Aelith' AND fightStatus = 3
+    WHERE idFighter = fighters.name AND fightStatus = 3
   )                                        AS forfeits,
   (
     SELECT Count(af.idFight)
@@ -66,13 +66,13 @@ SELECT
       LEFT JOIN nsfw_fights f
         ON af.idFight = f.idFight
     WHERE f.season = 1
-          AND idFighter = 'Aelith'
+          AND idFighter = fighters.name
           AND fightStatus = 3
   )                                        AS forfeitsSeason,
   (
     SELECT Count(idFight)
     FROM nsfw_activefighters
-    WHERE idFighter = 'Aelith' AND fightStatus = 4
+    WHERE idFighter = fighters.name AND fightStatus = 4
   )                                        AS fightsPendingReady,
   (
     SELECT Count(af.idFight)
@@ -80,13 +80,13 @@ SELECT
       LEFT JOIN nsfw_fights f
         ON af.idFight = f.idFight
     WHERE f.season = 1
-          AND idFighter = 'Aelith'
+          AND idFighter = fighters.name
           AND fightStatus = 4
   )                                        AS fightsPendingReadySeason,
   (
     SELECT Count(af.idFight)
     FROM nsfw_activefighters af
-    WHERE idFighter = 'Aelith' AND fightStatus = 5
+    WHERE idFighter = fighters.name AND fightStatus = 5
   )                                        AS fightsPendingStart,
   (
     SELECT Count(af.idFight)
@@ -94,13 +94,13 @@ SELECT
       LEFT JOIN nsfw_fights f
         ON af.idFight = f.idFight
     WHERE f.season = 1
-          AND idFighter = 'Aelith'
+          AND idFighter = fighters.name
           AND fightStatus = 5
   )                                        AS fightsPendingStartSeason,
   (
     SELECT Count(af.idFight)
     FROM nsfw_activefighters af
-    WHERE idFighter = 'Aelith' AND af.wantsDraw = TRUE
+    WHERE idFighter = fighters.name AND af.wantsDraw = TRUE
   )                                        AS fightsPendingDraw,
   (
     SELECT Count(af.idFight)
@@ -108,153 +108,172 @@ SELECT
       LEFT JOIN nsfw_fights f
         ON af.idFight = f.idFight
     WHERE f.season = 1
-          AND idFighter = 'Aelith'
+          AND idFighter = fighters.name
           AND af.wantsDraw = TRUE
   )                                        AS fightsPendingDrawSeason,
   (
     SELECT assignedTeam
     FROM nsfw_activefighters
-    WHERE idFighter = 'Aelith'
+    WHERE idFighter = fighters.name
     GROUP BY assignedTeam
     ORDER BY Count(*) DESC
     LIMIT 1
   )                                        AS favoriteTeam,
   (
-    SELECT idFighter
-    FROM nsfw_activefighters
-    WHERE idFighter <> 'Aelith'
-    GROUP BY assignedTeam
+    SELECT af.idFighter
+    FROM nsfw_activefighters af
+      INNER JOIN nsfw_activefighters ax
+        ON af.idFight = ax.idFight
+           AND af.assignedTeam = ax.assignedTeam
+    WHERE ax.idFighter = fighters.name
+          AND af.idFighter <> fighters.name
+    GROUP BY af.idFighter
     ORDER BY Count(*) DESC
     LIMIT 1
   )                                        AS favoriteTagPartner,
+  (
+    SELECT Count(*)
+    FROM nsfw_activefighters af
+      INNER JOIN nsfw_activefighters ax
+        ON af.idFight = ax.idFight
+           AND af.assignedTeam = ax.assignedTeam
+    WHERE ax.idFighter = fighters.name
+          AND af.idFighter <> fighters.name
+    GROUP BY af.idFighter
+    ORDER BY Count(*) DESC
+    LIMIT 1
+  )                                        AS timesFoughtWithFavoriteTagPartner,
   IFNULL((SELECT AVG(diceScore)
           FROM nsfw_actions
-          WHERE idAttacker = 'Aelith'), 0) AS averageDiceRoll,
+          WHERE idAttacker = fighters.name), 0) AS averageDiceRoll,
   (
     SELECT Count(missed)
     FROM nsfw_actions
-    WHERE idAttacker = 'Aelith'
+    WHERE idAttacker = fighters.name
   )                                        AS missedAttacks,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE idAttacker = 'Aelith'
+    WHERE idAttacker = fighters.name
   )                                        AS actionsCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE idDefender = 'Aelith'
+    WHERE idDefender = fighters.name
   )                                        AS actionsDefended,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 0 AND (idAttacker = 'Aelith') AND missed = FALSE
+    WHERE type = 0 AND (idAttacker = fighters.name) AND missed = FALSE
   )                                        AS brawlAtksCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 1 AND missed = FALSE AND (idAttacker = 'Aelith')
+    WHERE type = 1 AND missed = FALSE AND (idAttacker = fighters.name)
   )                                        AS sexstrikesCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 2 AND missed = FALSE AND (idAttacker = 'Aelith' OR idDefender = 'Aelith')
+    WHERE type = 2 AND missed = FALSE AND (idAttacker = fighters.name OR idDefender = fighters.name)
   )                                        AS tagsCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 3 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 3 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS restCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 4 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 4 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS subholdCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 5 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 5 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS sexholdCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 6 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 6 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS bondageCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 7 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 7 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS humholdCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 8 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 8 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS itemPickups,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 9 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 9 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS sextoyPickups,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 10 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 10 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS degradationCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 11 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 11 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS forcedWorshipCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 12 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 12 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS highriskCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 13 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 13 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS penetrationCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 14 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 14 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS stunCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 15 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 15 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS escapeCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 16 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 16 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS submitCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 17 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 17 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS straptoyCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 18 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 18 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS finishCount,
   (
     SELECT Count(*)
     FROM nsfw_actions
-    WHERE type = 19 AND missed = FALSE AND idAttacker = 'Aelith'
+    WHERE type = 19 AND missed = FALSE AND idAttacker = fighters.name
   )                                        AS masturbateCount,
   (
-    SELECT Count(idFight) AS fights
+    SELECT Count(idFight)
     FROM nsfw_activefighters af
-    WHERE idFighter = 'Aelith' AND timediff(NOW(), af.updatedAt) < '24:00:00'
+    WHERE idFighter = fighters.name AND timestampdiff(HOUR, af.updatedAt, NOW()) <= 24
   )                                        AS matchesInLast24Hours,
   (
-    SELECT Count(idFight) AS fights
+    SELECT Count(idFight)
     FROM nsfw_activefighters af
-    WHERE idFighter = 'Aelith' AND timediff(NOW(), af.updatedAt) < '48:00:00'
-  )                                        AS matchesInLast48Hours;
+    WHERE idFighter = fighters.name AND timestampdiff(HOUR, af.updatedAt, NOW()) <= 48
+  )                                        AS matchesInLast48Hours
+FROM nsfw_fighters fighters, nsfw_constants consts
+WHERE consts.name = 'currentSeason';
+
 
 
 
