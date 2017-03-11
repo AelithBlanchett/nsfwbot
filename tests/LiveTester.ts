728dfbf9a662d509f148b64001b84a7039ad3d5e
AelithBlanchett <tonyteur@yahoo.fr>
2017-03-11 12:53:08 -0500

    Update package.json

---
+++ tests/LiveTester.ts
@@ -0,0 +1,3 @@
+let FChatLib = require('fchatlib');
+let options = {username: "tonyteur", password: "aaa", character: "Battle-fuck Arena", master: "Aelith Blanchette", room: "ADH-c7188539de793c27ddf8"};
+let myFchatBot = new FChatLib(options);
